9ef9e8d9728ce9a6390d8a23d1a1bfa1c05c13db
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2021-06-13 01:14:03 +0300

    add

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager
+{
+    private int a;
+    public int A => a;
+
+    public Manager(int _a)
+    {
+        a = _a;
+    }
+
+    public int Add(int a)
+    {
+        a = a + 2;
+        return a;
+    }
+}
+
+
---
+++ Assets/Scripts/UnityManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityManager : MonoBehaviour
+{
+    public Button btn;
+    public Text _t;
+
+    private void OnEnable()
+    {
+        btn.onClick.AddListener(Calc);
+    }
+
+    private void Calc()
+    {
+        Manager m = new Manager(2);
+        _t.text = m.A.ToString() + "/" + m.Add(2).ToString();
+    }
+
+    private void OnDisable()
+    {
+        btn.onClick.RemoveListener(Calc);
+    }
+}
